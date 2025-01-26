ad889db57e1196723c652e2fbbed1d5bedb03695
astra0081. <sneer-anyhow-jump@duck.com>
2025-01-26 12:47:43 +0530

    Update menu.lua

--- builtin/client/menu.lua
+++ builtin/client/menu.lua
@@ -19,8 +19,8 @@ local settings = {
     {"Killaura", "Killaura"},
     {"autoaim", "Aimbot"},
     {"autohit", "Auto Hit"},
-    {"dodge", "Dodge (K)"},
-    {"autoescape", "Auto Escape (K)"},
+   -- {"dodge", "Dodge (K)"},
+    --{"autoescape", "Auto Escape (K)"},
     {"noknockback", "No Knockback"},
 
     -- Movement Cheats
@@ -174,12 +174,7 @@ minetest.register_on_formspec_input(function(formname, fields)
     if fields["toggle_Killaura"] == "true" then
         minetest.display_chat_message(minetest.colorize("#FFFDD0", "Info: Addons of killaura end with (K). They require killaura to be turned on to work."))
     end
-    if fields["toggle_dodge"] == "true" and fields["toggle_Killaura"] then
-        minetest.display_chat_message(minetest.colorize("#FFFDD0", "Info: Dodge randomly teleports you to the enemy in a 6 block radius so it is hard for the enemy to hit you. This is easily detectable and makes killaura a bit slow so use it at your own risk."))
-    end
-    if fields["toggle_autoescape"] == "true" then
-        minetest.display_chat_message(minetest.colorize("#FFFDD0", "Info: Autoescape teleports you below the ground if the health is below 5. (Works only when dodge is turned on.)"))
-    end
+
     if fields["toggle_custom_sky"] == "true" then
         minetest.settings:set("Sun", "false")
         minetest.settings:set("Moon", "false")
